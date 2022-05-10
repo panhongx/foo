8efdedd82e58bea023ab03e77534c107c88db3ad
Hongxi Pan <panhxi@gmail.com>
2022-05-10 16:13:32 +0800

    First commit

---
+++ src/main/java/org/example/Foo.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Foo {
+    /**
+     * ding dong
+     * @param size size of the xxxxx
+     * @return true if xxx
+     * @throws  IllegalArgumentException thrown if xxxx
+     */
+    public boolean bar(int size) {
+        return true;
+    }
+}
